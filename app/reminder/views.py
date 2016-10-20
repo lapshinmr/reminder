7e7ee20ab8213ebc8bc508b698927c9d727648d7
Mikhail <lapshinmr@gmail.com>
2016-10-20 10:49:22 +0300

    reminder: create buttons

--- app/reminder/views.py
+++ app/reminder/views.py
@@ -1,8 +1,13 @@
 from . import reminder
-from flask import render_template
+from flask import render_template, redirect, url_for
 
 
 @reminder.route('/reminder')
 def index():
     return render_template('reminder/index.html')
 
+
+@reminder.route('/reminder/press/<button>')
+def press(button):
+    print(button)
+    return redirect(url_for('reminder.index'))
