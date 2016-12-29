6367337cbc3474d0b47c46459a8393d21db477db
Mikhail <lapshinmr@gmail.com>
2016-12-29 18:03:01 +0400

    create clickable timetable

--- app/static/duration.js
+++ app/static/duration.js
@@ -2,7 +2,7 @@
 
 //view
 var view = {
-  createTimeUnit: function(id, value) {
+  createTimeUnit: function(id) {
     var unit = document.createElement('div');
     unit.setAttribute('id', id);
     unit.innerHTML = (
@@ -29,7 +29,7 @@ var view = {
       fontWeight: "900"
     });
     $( element ).stop().animate({
-        color: "rgb(150, 150, 150)",
+        color: "rgb(100, 100, 100)",
         fontWeight: "400",
         queue: false
       }, 300
@@ -70,7 +70,6 @@ TimeUnit.prototype.increase = function() {
 var model = {
   units: [ ],
 
-
   getTime: function() {
     var time = 0;
     for (var i = 0; i < this.units.length; i++) {
@@ -110,6 +109,79 @@ var model = {
     })
   },
 
+  switchFromParToInput: function(id) {
+    var element = $('#' + id).find('.count');
+    var new_element = $('<input>', {
+        type: 'text',
+        class: $(element).attr('class'),
+        value: $(element).text()
+      }
+    );
+    element.replaceWith($(new_element));
+    var value = new_element.val();
+    var value_length = value.length;
+    new_element.focus();
+    new_element[0].setSelectionRange(value_length, value_length);
+    $(new_element).focusout(
+      function() {
+        if (isNaN($(new_element).val())) {
+          $(new_element).val(value);
+        };
+        model.switchFromInputToPar(id);
+      }
+    );
+    $(new_element).keypress(
+      function(e) {
+        if (e.which == 13) {
+          if (isNaN($(new_element).val())) {
+            $(new_element).val(value);
+          };
+          model.switchFromInputToPar(id);
+        }
+      }
+    );
+    $(new_element).on('mousewheel DOMMouseScroll',
+      function(event) {
+        if (isNaN($(new_element).val())) {
+          $(new_element).val(value);
+        };
+        model.switchFromInputToPar(id);
+      }
+    );
+  },
+
+  switchFromInputToPar: function(id) {
+    var element = $('#' + id).find('.count');
+    var new_element = $('<p>', {
+      class: $(element).attr('class'),
+    });
+    var value = element.val();
+    for (var i = 0; i < this.units.length; i++) {
+      var unit = this.units[i];
+      if (unit.id == id) {
+        if (value > unit.maxValue) {
+          var value = unit.maxValue;
+        }
+        this.units[i].value = value;
+      }
+    };
+    $(new_element).text(value);
+    element.replaceWith($(new_element));
+    $(new_element).click(
+      function() {
+        model.switchFromParToInput(id);
+      }
+    )
+  },
+
+  attachClickToCount: function(id) {
+      $('#' + id).find('.count').click(
+        function(){
+          model.switchFromParToInput(id)
+        }
+      );
+  },
+
   createTimeTable: function() {
     var duration = document.getElementById("duration-picker");
     var input = document.createElement('input');
@@ -130,6 +202,7 @@ var model = {
       this.attachClicksToArrows(unitModel, unitView);
       this.attachScrollToUnit(unitModel, unitView);
       duration.appendChild(unitView);
+      this.attachClickToCount(id);
       view.updateTimeUnitValue(id, value, zeroes);
       view.createSeparator(id, value, after);
     }
