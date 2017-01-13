9bf11d65ff8c8619b451bb7d9afc915c584faeaa
Mikhail <lapshinmr@gmail.com>
2017-01-13 16:40:19 +0400

    edit task name and add task with ajax

--- app/reminder/views.py
+++ app/reminder/views.py
@@ -1,28 +1,14 @@
 from app import db
 from . import reminder
 from .models import Task, Time
-from flask import render_template, redirect, url_for, request
+from flask import render_template, redirect, url_for, request, jsonify
 import datetime
 from .reminder_tools import TimeUnitsRanges
 
 
 @reminder.route('/reminder', methods=['GET', 'POST'])
 def index():
-    if 'add_task_submit' in request.form:
-        task_name = request.form['task_name']
-        print(request.form)
-        time_loop = int(request.form['duration'])
-        task = Task.query.filter_by(name=task_name).first()
-        if not task:
-            new_task = Task(name=task_name, time_loop=time_loop)
-            db.session.add(new_task)
-        elif task.time_close:
-            task.update(time_loop)
-            task.restore()
-        db.session.commit()
-        return redirect(url_for('reminder.index'))
     tasks = Task.query.all()
-    tasks = sorted(tasks, key=Task.count_left_time)
     tasks_times = [(Task.query.filter_by(id=time.task_id).first(), time.time_complete) for time in Time.query.all()]
     return render_template(
         'reminder/index.html', tasks=tasks, tasks_times=tasks_times,
@@ -30,9 +16,33 @@ def index():
     )
 
 
-@reminder.route('/reminder/<task_name>/complete')
-def complete(task_name):
-    task = Task.query.filter_by(name=task_name).first()
+@reminder.route('/reminder/add_task', methods=['POST'])
+def add():
+    print(request.form)
+    task_name = request.form['task-name']
+    time_loop = int(request.form['duration'])
+    new_task = Task(name=task_name, time_loop=time_loop)
+    db.session.add(new_task)
+    db.session.commit()
+    tasks = Task.query.all()
+    return jsonify({
+        'tasks': render_template('reminder/tasks_area.html', tasks=tasks)
+    })
+
+
+@reminder.route('/reminder/<task_id>/edit', methods=['GET', 'POST'])
+def edit(task_id):
+    task = Task.query.filter_by(id=task_id).first()
+    new_task_name = request.form.get('new_task_name')
+    if task.name != new_task_name:
+        task.name = new_task_name
+        db.session.commit()
+    return jsonify()
+
+
+@reminder.route('/reminder/<task_id>/complete')
+def complete(task_id):
+    task = Task.query.filter_by(id=task_id).first()
     time_complete = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     if task:
         time = Time(task=task)
@@ -42,9 +52,9 @@ def complete(task_name):
     return redirect(url_for('reminder.index'))
 
 
-@reminder.route('/reminder/<task_name>/close')
-def close(task_name):
-    task = Task.query.filter_by(name=task_name).first()
+@reminder.route('/reminder/<task_id>/close')
+def close(task_id):
+    task = Task.query.filter_by(name=task_id).first()
     if task:
         times = Time.query.filter_by(task=task).all()
         if not any(times):
@@ -54,9 +64,9 @@ def close(task_name):
     return redirect(url_for('reminder.index'))
 
 
-@reminder.route('/reminder/<task_name>/<time_complete>/remove')
-def remove(task_name, time_complete):
-    task = Task.query.filter_by(name=task_name).first()
+@reminder.route('/reminder/<task_id>/<time_complete>/remove')
+def remove(task_id, time_complete):
+    task = Task.query.filter_by(name=task_id).first()
     if task:
         time = Time.query.filter_by(task=task).filter_by(time_complete=time_complete).first()
         if time:
@@ -68,9 +78,9 @@ def remove(task_name, time_complete):
     return redirect(url_for('reminder.index'))
 
 
-@reminder.route('/reminder/<task_name>/restore')
-def restore(task_name):
-    task = Task.query.filter_by(name=task_name).first()
+@reminder.route('/reminder/<task_id>/restore')
+def restore(task_id):
+    task = Task.query.filter_by(name=task_id).first()
     if task:
         task.time_close = None
         db.session.commit()
