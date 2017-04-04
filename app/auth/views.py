c4d26204556ba82b6050ee4e9e8e2d88887bed8a
Mikhail <lapshinmr@gmail.com>
2017-04-04 14:41:36 +0400

    add flash messages

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,5 +1,5 @@
 import os
-from flask import request, url_for, redirect, render_template
+from flask import request, url_for, redirect, render_template, flash
 from flask_login import login_user, login_required, logout_user, current_user
 from . import auth
 from app.models import User
@@ -8,18 +8,37 @@ from app.celery_tasks import send_async_email
 from app.utils import make_subject
 
 
+@auth.before_app_request
+def before_request():
+    if current_user.is_authenticated \
+            and not current_user.confirmed \
+            and request.endpoint[:5] != 'auth.' \
+            and request.endpoint != 'static':
+        return redirect(url_for('auth.unconfirmed'))
+
+
+@auth.route('/unconfirmed')
+def unconfirmed():
+    if current_user.is_anonymous or current_user.confirmed:
+        return redirect(url_for('main.index'))
+    return render_template('auth/unconfirmed.html')
+
+
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    email = request.form.get('email')
-    password = request.form.get('password')
-    remember_me = request.form.get('remember_me')
-    remember_me = True if remember_me else False
-    user = User.query.filter_by(email=email).first()
-    if user is not None and user.verify_password(password):
-        login_user(user, remember_me)
-        return redirect(request.args.get('next') or url_for('main.index'))
-    cur_config = os.environ.get('CONFIG')
-    return render_template('auth/login.html', config=cur_config)
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        remember_me = request.form.get('remember_me')
+        remember_me = True if remember_me else False
+        user = User.query.filter_by(email=email).first()
+        if user is not None and user.verify_password(password):
+            login_user(user, remember_me)
+            print(request.args.get('next'))
+            return redirect(request.args.get('next') or url_for('main.index'))
+        else:
+            flash('Please, use your email and password to log in.')
+    return render_template('auth/login.html', config=os.environ.get('CONFIG'))
 
 
 @auth.route('/logout', methods=['GET', 'POST'])
@@ -48,6 +67,8 @@ def signup():
                     message_text
                 ]
             )
+            flash('Email with confirmation has been sent to your email address. Please go to your mailbox and use link'
+                  'to confirm your account.')
         return redirect(url_for('auth.login'))
     cur_config = os.environ.get('CONFIG')
     return render_template('auth/signup.html', config=cur_config)
